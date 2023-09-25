4740962001606e2d53f7faf7c06c7bc5e0caba41
George Focas <grfocas@gmail.com>
2023-09-25 17:49:03 +0100

    Create help.js
    adiciona arquivo para ajuda

---
+++ ajuda/help.js
@@ -0,0 +1,37 @@
+# somar duas variáveis
+valor1 = 2
+valor2 = 3
+
+resultado = valor1 + valor2
+
+# diminuir duas variáveis
+valor1 = 2
+valor2 = 3
+
+resultado = valor1 - valor2
+
+# multiplicar duas variáveis
+valor1 = 2
+valor2 = 3
+
+resultado = valor1 * valor2
+
+# dividir duas variáveis
+
+valor1 = 2
+valor2 = 3
+
+resultado = valor1 / valor2
+
+# criar uma variável do tipo string
+
+meu_texto = 'Isto é um texto'
+
+# comando para mostrar alguma coisa na tela
+print('um texto aqui') 
+
+# pode ser número também
+valor1 = 1
+
+print(valor1)
+print(23)
